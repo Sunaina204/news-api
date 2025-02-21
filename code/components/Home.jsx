c88d0faf4447751b67d9ae89e2400d531361a974
Sunaina204 <sunainamarwaha8@gmail.com>
2025-02-21 16:11:02 +0530

    add articles array 0 condition

--- code/components/Home.jsx
+++ code/components/Home.jsx
@@ -1,6 +1,4 @@
-'use client';
-import Image from "next/image";
-
+"use client";
 import { useState, useEffect } from "react";
 import NavbarC from "./Navbar";
 import Card from "./Card";
@@ -8,14 +6,15 @@ import Card from "./Card";
 export default function Home() {
   const [articles, setarticles] = useState([]);
 
- const[category, setcategory] = useState('general');
-// console.log(process.env.DB);
+  const [category, setcategory] = useState("general");
+  // console.log(process.env.DB);
 
   useEffect(() => {
-    console.log('useeffecct is running');
-    
+    console.log("useeffecct is running");
+
     fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8e31d0e7c02d4eab88089a82294a45e0`)
+      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8e31d0e7c02d4eab88089a82294a45e0`
+    )
       .then((data) => data.json())
       .then((result) => setarticles(result.articles))
       .catch((err) => {
@@ -24,30 +23,30 @@ export default function Home() {
   }, [category]);
   // console.log(category);
 
-
   return (
     <>
-    <NavbarC category={setcategory} />
-    <div className="bgcolor">
-    <div className="container">
-      <div className="row">
-      <h1 className="text-center text-white">News - Top {category} Headlines</h1>
-        {articles.map((item, index) => {
-          return (
-            
-  
-            <Card
-              key={index}
-              title={item.title}
-              desc={item.description}
-              img={item.urlToImage}
-              url={item.url}
-            />
-          );
-        })}
+      <NavbarC category={setcategory} />
+      <div className="bgcolor">
+        <div className="container">
+          <div className="row">
+            <h1 className="text-center text-white">
+              News - Top {category} Headlines
+            </h1>
+            {articles.length != 0 &&
+              articles.map((item, index) => {
+                return (
+                  <Card
+                    key={index}
+                    title={item.title}
+                    desc={item.description}
+                    img={item.urlToImage}
+                    url={item.url}
+                  />
+                );
+              })}
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-  </>
+    </>
   );
 }
