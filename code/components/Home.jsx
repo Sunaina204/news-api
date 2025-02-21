65d2e827dc5c635c755722beb701791892c528bc
Sunaina204 <sunainamarwaha8@gmail.com>
2025-02-21 16:13:29 +0530

    update

--- code/components/Home.jsx
+++ code/components/Home.jsx
@@ -4,7 +4,19 @@ import NavbarC from "./Navbar";
 import Card from "./Card";
 
 export default function Home() {
-  const [articles, setarticles] = useState([]);
+  const [articles, setarticles] = useState([{
+    "source": {
+      "id": null,
+      "name": "Newser"
+    },
+    "author": "Rob Quinn",
+    "title": "Thousands Rescued From Notorious Scam Compounds - Newser",
+    "description": "Victims lured to Thailand with job offers were forced to work at scam centers in Myanmar",
+    "url": "https://www.newser.com/story/364525/thousands-rescued-from-notorious-scam-compounds.html",
+    "urlToImage": "https://img1-azrcdn.newser.com/image/1593738-12-20250219182902.jpeg",
+    "publishedAt": "2025-02-20T08:15:03Z",
+    "content": "Thai authorities launched a fresh crackdown on the centers last month after Chinese actor Wang Xing was abducted in Bangkok, where he had been promised a lucrative acting job, Reuters reports. He was… [+900 chars]"
+  }]);
 
   const [category, setcategory] = useState("general");
   // console.log(process.env.DB);
